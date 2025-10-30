f8879b28866b1f4d58dd2181973125e4bb810c72
rebahama <97362222+rebahama@users.noreply.github.com>
2025-10-30 18:26:30 +0000

    add permission class for auth

--- diagnosis/views.py
+++ diagnosis/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import DiagnosisRequest
 from .serializers import DiagnosisResultSerializer
+from DRF_AI.permissions import IsOwnerOrReadOnly
 
 class DiagnosisRequestCreateView(generics.ListCreateAPIView):
     queryset = DiagnosisRequest.objects.all()
     serializer_class = DiagnosisResultSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    
+
 
 
 class DiagnosisDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = DiagnosisResultSerializer
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = DiagnosisRequest.objects.all()
