6acbe398a7db8ea254a9a480854a7e9b73be4af3
rebahama <97362222+rebahama@users.noreply.github.com>
2025-10-31 20:02:40 +0000

    add requ text

--- DRF_AI/urls.py
+++ DRF_AI/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import root_route, logout_route
 urlpatterns = [
+    path("", root_route),
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout', logout_route),
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
     path("", include('diagnosis.urls')),
