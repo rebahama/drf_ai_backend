8f91b9ebb38c30466f6484cf1ff0c4f68fb65498
rebahama <97362222+rebahama@users.noreply.github.com>
2025-11-02 23:33:47 +0000

    change origin

--- DRF_AI/settings.py
+++ DRF_AI/settings.py
@@ -34,9 +34,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
 
-if "CLIENT_ORIGIN" in os.environ:
+if "CLIENT_ORIGIN_DEV" in os.environ:
     extracted_url = re.match(
-        r"^.+-", os.environ.get("CLIENT_ORIGIN", ""), re.IGNORECASE
+        r"^.+-", os.environ.get("CLIENT_ORIGIN_DEV", ""), re.IGNORECASE
     ).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
