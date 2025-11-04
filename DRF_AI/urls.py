e66f9e1d97fd51e63060212fb35aebcfcad4c3df
rebahama <97362222+rebahama@users.noreply.github.com>
2025-11-04 02:29:20 +0000

    add logut

--- DRF_AI/urls.py
+++ DRF_AI/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route, logout_route
+from .views import root_route
 from rest_framework_simplejwt.views import TokenRefreshView
 
 
@@ -25,7 +25,6 @@ urlpatterns = [
     path("", root_route),
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('dj-rest-auth/logout', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path("dj-rest-auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
